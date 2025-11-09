7c4d8a1f35bbefde34595034ec46029b04c9dfb2
barrazaluis <cbarrazaluis@gmail.com>
2025-11-09 16:16:24 -0300

    data store

--- server.js
+++ server.js
@@ -6,5 +6,20 @@ const YAML = require('yamljs');
 
 const swaggerDocument = YAML.load('./docs/swagger.json');
 
-
 require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+
+app.use('/api/reservas', reservasRoutes);
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
+app.use((err, req, res, next) => {
+console.error(err);
+res.status(err.status || 500).json({ error: err.message || 'Internal Server Error' });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servicio corriendo en http://localhost:${PORT}`));
